3fe397c174e12ebed97477f3712c6bf09ba4e939
ns0851 <ns0851@srmist.edu.in>
2025-11-28 11:42:23 +0530

    in order traversal

--- manual_traversal.c
+++ manual_traversal.c
@@ -1,52 +1,123 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-struct TreeNode {
+struct TreeNode
+{
     int val;
     struct TreeNode *left;
     struct TreeNode *right;
 };
 
-struct TreeNode *insert(struct TreeNode *node, int data) {
-    if(node == NULL) {
+struct TreeNode *insert(struct TreeNode *node, int data)
+{
+    if (node == NULL)
+    {
         struct TreeNode *newnode = malloc(sizeof(struct TreeNode));
         newnode->val = data;
         newnode->left = newnode->right = NULL;
         return newnode;
     }
-    if(node->val > data) {
+    if (node->val > data)
+    {
         node->left = insert(node->left, data);
-    } else {
+    }
+    else
+    {
         node->right = insert(node->right, data);
     }
     return node;
 }
 
-void preOrderManual(struct TreeNode *node) {
-    struct TreeNode *stack[100];
-    int top = -1;
-    int current = 0;
-    stack[++top] = node;  
-    while(top!=-1) {
-        struct TreeNode *currentNode = stack[top];
-        printf("%d ->", currentNode->val);
-        currentNode = stack[top--];
-        if(currentNode->right != NULL) stack[++top] = currentNode->right;
-        if(currentNode->left != NULL) stack[++top] = currentNode->left;
-    }
+int getCount(struct TreeNode *node)
+{
+    if (node == NULL)
+        return 0;
+    return 1 + getCount(node->left) + getCount(node->right);
 }
 
-void freeTree(struct TreeNode *node) {
-    if(!node) return;
+// -----------------------------------------Pre Order Traversal------------------------------------------ //
+void preOrderManual(struct TreeNode *node)
+{
+    int count = getCount(node);
+    struct TreeNode **stack = malloc(count * sizeof(struct TreeNode *));
+    int top = -1;
+    int current = 0;
+    stack[++top] = node;
+    while (top != -1)
+    {
+        struct TreeNode *currentNode = stack[top];
+        printf("%d -> ", currentNode->val);
+        top--;
+        if (currentNode->right != NULL)
+            stack[++top] = currentNode->right;
+        if (currentNode->left != NULL)
+            stack[++top] = currentNode->left;
+    }
+    free(stack);
+}
+
+// --------------------------------------End of Pre Order Traversal-------------------------------------- //
+
+bool isElementPresent(int count, struct TreeNode *arr[], struct TreeNode *data) {
+    for (int i = 0; i < count; i++) {
+        if (arr[i] != NULL && (arr[i]->val == data->val)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// --------------------------In Order Manual Recursion Simulation (Custom Approach)---------------------- //
+void inOrderManual(struct TreeNode *node) {
+    int count = getCount(node);
+    struct TreeNode **stack = calloc(count, sizeof(struct TreeNode *));
+    struct TreeNode **visited = calloc(count, sizeof(struct TreeNode *));
+    int top = -1;
+    int visitedIndex = -1;
+    stack[++top] = node;
+    while (top != -1) {
+        struct TreeNode *currentNode = stack[top];
+        if (isElementPresent(count, visited, currentNode)) {
+            // pop current node
+            printf("%d -> ", currentNode->val);
+            stack[top] = NULL;
+            top--;
+            // push right node
+            if (currentNode->right != NULL)
+                stack[++top] = currentNode->right;
+        }
+        else {
+            if (currentNode != NULL) {
+                // push current node to visited
+                visited[++visitedIndex] = currentNode;
+                // push left node
+                if (currentNode->left != NULL)
+                    stack[++top] = currentNode->left;
+            }
+        }
+    }
+    free(stack);
+    free(visited);
+}
+
+// ----------------------End of In Order Manual Recursion Simulation (Custom Approach)------------------- //
+
+void freeTree(struct TreeNode *node)
+{
+    if (!node)
+        return;
     freeTree(node->left);
     freeTree(node->right);
     free(node);
 }
 
-int main() {
+int main()
+{
     struct TreeNode *node = malloc(sizeof(struct TreeNode));
-    if (node == NULL) {
-        return 0; 
+    if (node == NULL)
+    {
+        return 0;
     }
     node->val = 5;
     node->left = NULL;
@@ -58,9 +129,10 @@ int main() {
     insert(node, 3);
     insert(node, 7);
 
-    printf("\n");
-
+    printf("\nPre Order Traversal: ");
     preOrderManual(node);
+    printf("\nIn Order Traversal: ");
+    inOrderManual(node);
 
     freeTree(node);
     return 0;
